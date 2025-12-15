d16ce28babbd2ed58731123dfb78ce6226b7ad4f
vincentcanayan1 <vincentcanayan1@gmail.com>
2025-12-16 07:02:27 +0800

    made class for user repositories

---
+++ OOPBorrowingSystem/User.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBorrowingSystem
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public int BorrowNumber { get; set; }
+        public string BorrowedItems { get; set; }
+        public string BorrowDate { get; set; }
+        public bool Returned { get; set; }
+    }
+}
---
+++ OOPBorrowingSystem/UserRepository.cs
@@ -0,0 +1,70 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBorrowingSystem
+{
+    public class UserRepository
+    {
+        private readonly ISQLiteConnection _connection;
+
+        public UserRepository()
+        {
+            var databasePath = Path.Combine(
+                Environment.GetFolderPath(
+                    Environment.SpecialFolder.MyDocuments),
+                "userdata.db");
+
+
+            _connection = new SQLiteConnection(databasePath);
+            _connection.CreateTable<User>();
+
+
+        }
+        public bool Add(User user)
+        {
+
+            _connection.Insert(user);
+            return true;
+        }
+        public bool Update(User user)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection("userdata.db"))
+                {
+
+                    var result = connection.Execute(
+                        "UPDATE users SET IsReturned = ? WHERE Id = ?",
+                        user.Returned ? 1 : 0,
+                        user.Id
+                    );
+
+                    return result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public List<User> GetAll()
+        {
+
+            return _connection.Table<User>().ToList();
+        }
+
+        public User Get(int id)
+        {
+            return _connection.Find<User>(id);
+        }
+
+    }
+}
