7e8553afd24fdd7c4832ea5bf1554d702dc73fb7
vincentcanayan1 <vincentcanayan1@gmail.com>
2025-12-16 06:47:27 +0800

    made new forms

--- OOPBorrowingSystem/Form1.Designer.cs
+++ OOPBorrowingSystem/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OOPBorrowingSystem
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.button2Cancel = new System.Windows.Forms.Button();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(444, 214);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Log in";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // button2Cancel
+            // 
+            this.button2Cancel.Location = new System.Drawing.Point(363, 214);
+            this.button2Cancel.Name = "button2Cancel";
+            this.button2Cancel.Size = new System.Drawing.Size(75, 23);
+            this.button2Cancel.TabIndex = 1;
+            this.button2Cancel.Text = "Cancel";
+            this.button2Cancel.UseVisualStyleBackColor = true;
+            this.button2Cancel.Click += new System.EventHandler(this.button2Cancel_Click);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(389, 158);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 22);
+            this.textBoxUsername.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(389, 186);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(316, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.button2Cancel);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "LoginForm";
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button button2Cancel;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- OOPBorrowingSystem/Form1.cs
+++ OOPBorrowingSystem/Form1.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
 
 namespace OOPBorrowingSystem
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if ((textBoxUsername.Text == "admin" && textBoxPassword.Text == "123") ||
+                       (textBoxUsername.Text == "radley" && textBoxPassword.Text == "123") ||
+                       (textBoxUsername.Text == "hans" && textBoxPassword.Text == "123") ||
+                       (textBoxUsername.Text == "vincent" && textBoxPassword.Text == "123"))
+            {
+                MessageBox.Show("Login Successful!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Selection form2 = new Selection();
+                form2.Show();
+                this.Hide();
+                form2.FormClosed += (s, args) => this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void button2Cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
